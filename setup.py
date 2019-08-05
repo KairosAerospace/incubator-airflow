0551afa9c5bd69d9fad20c8a5d64a881159da827
Belinda Chin <belinda@kairosaerospace.com>
2019-08-05 12:13:23 -0700

    Change tenacity to be >= 4.12.0 and bump name to kairflow3.

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ else:
 def do_setup():
     write_version()
     setup(
-        name='kairflow2',
+        name='kairflow3',
         description='Programmatically author, schedule and monitor data pipelines',
         long_description=long_description,
         long_description_content_type='text/markdown',
@@ -320,7 +320,7 @@ def do_setup():
             'setproctitle>=1.1.8, <2',
             'sqlalchemy>=1.1.15, <1.3.0',
             'tabulate>=0.7.5, <0.9',
-            'tenacity==4.12.0',
+            'tenacity>=4.12.0',
             'text-unidecode==1.2',
             'typing;python_version<"3.5"',
             'thrift>=0.9.2',
