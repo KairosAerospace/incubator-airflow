9821d967eb5374bb0ea588a82e4ffcb10b519dfc
belinda <belinda@kairosaerospace.com>
2020-12-09 12:14:22 -0800

    Change package name back to kairflow3

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ else:
 def do_setup():
     write_version()
     setup(
-        name='kairflow4',
+        name='kairflow3',
         description='Programmatically author, schedule and monitor data pipelines',
         long_description=long_description,
         long_description_content_type='text/markdown',
